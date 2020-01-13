2dad826dc3248f1027059a006c9cf33aaf316f80
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-13 16:33:19 -0600

    -Created a basic schema with no references.

--- data/migrations/20200113155011_sprint.js
+++ data/migrations/20200113155011_sprint.js
@@ -1,8 +1,28 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable('projects', tbl => {
+      tbl.increments();
+      tbl.string('name', 128).notNullable();
+      tbl.string('description', 255);
+      tbl.integer('completed').notNullable().defaultTo(0);
+    })
+    .createTable('resources', tbl => {
+        tbl.increments();
+        tbl.string('description', 255).notNullable();
+        tbl.string('description', 255);
+    })
+    .createTable('tasks', tbl => {
+        tbl.increments();
+        tbl.string('name', 128).notNullable();
+        tbl.string('notes', 255);
+        tbl.integer('completed').notNullable().defaultTo(0);
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects');
 };
