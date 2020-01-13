be87855b2cf1f13f86f999aa437da2beb9223748
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-13 16:53:26 -0600

    -Changed column names to more readable values. -Verified that rollback and latest migration is working.

--- data/migrations/20200113155011_sprint.js
+++ data/migrations/20200113155011_sprint.js
@@ -3,21 +3,21 @@ exports.up = function(knex) {
     return knex.schema
     .createTable('projects', tbl => {
       tbl.increments();
-      tbl.string('name', 128).notNullable();
-      tbl.string('description', 255);
+      tbl.string('project_name', 128).notNullable();
+      tbl.string('project_description', 255);
       tbl.integer('completed').notNullable().defaultTo(0);
     })
     .createTable('resources', tbl => {
         tbl.increments();
-        tbl.string('description', 255).notNullable();
-        tbl.string('description', 255);
+        tbl.string('resource_name', 255);
+        tbl.string('resource_description', 255).notNullable();
     })
     .createTable('tasks', tbl => {
         tbl.increments();
-        tbl.string('name', 128).notNullable();
-        tbl.string('notes', 255);
+        tbl.string('task_name', 128).notNullable();
+        tbl.string('task_notes', 255);
         tbl.integer('completed').notNullable().defaultTo(0);
-        
+
     })
 };
 
