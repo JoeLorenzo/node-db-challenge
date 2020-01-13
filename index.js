b0467eba1f4ebaceef4ebf63a176348bfdc3e030
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-13 15:05:48 -0600

    -Fixed dependency misspelling of knex in json.
    -Initalied knex config file.
    
    -Created boilerplate express server.
    
    -Root get functional.

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const server = express()
+
+
+server.get("/", (req, res) => {
+    res.status(200).send("Hello World, from the root path")
+})
+host = "127.0.0.1"
+port = 8080
+
+server.listen(port, ()=> console.log(`server running on ${host}:${port}, better go catch it...`)) 
