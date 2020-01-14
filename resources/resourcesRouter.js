57f7f2af51c124b1c8f06cb0ff9b02c211b12ad2
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-13 21:30:46 -0600

    -Get all resources routes functional.

--- resources/resourcesRouter.js
+++ resources/resourcesRouter.js
@@ -1,3 +1,20 @@
-const router = require('express').Router();
 
+const express = require("express")
+const router = require('express').Router();
+const projects = require('./resourcesModels')
+
+
+router.use(express.json())
+
+// This route gets all projects
+router.get("/resources", (req, res) => {
+    projects.get()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error:"Server error. Unable to retrieve projects"})
+    })
+})
 module.exports = router;
