fdc8ea6aa1102d99794a22fdd35a15535a742e24
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-14 18:43:16 -0600

    -Post project route functional.

--- projects/projectsModels.js
+++ projects/projectsModels.js
@@ -8,7 +8,15 @@ function getById(id) {
   return db('projects').where({id}).first();
 }
 
+function insert(project) {
+  return db('projects')
+    .insert(project)
+    .then(([id]) => getById(id));
+}
+
+
 module.exports = {
   get,
-  getById
+  getById,
+  insert
 };
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -15,6 +15,25 @@ router.get("/projects", (req, res) => {
     })
 })
 
+// This route post's a new project
+router.post("/projects", (req, res) => {
+    const { project_name, description_name } = req.body
+    if (project_name){
+        projects.insert(req.body)
+        .then(project => { 
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({err:"project could not be created"})
+        })
+    }
+   else {
+    res.status(400).json({error:"missing name or description"})
+   }
+})
+
+
 
 // This route get a specfic project by id
 router.get("/project/:projectId", (req, res) => {
